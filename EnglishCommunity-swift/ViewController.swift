c7102819d67cbab12ba77755ef5c7c1a0155f3cc
zhoujianfeng <admin@6ag.cn>
2016-08-01 21:27:33 +0800

    Initial Commit

---
+++ EnglishCommunity-swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EnglishCommunity-swift
+//
+//  Created by zhoujianfeng on 16/8/1.
+//  Copyright © 2016年 zhoujianfeng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
